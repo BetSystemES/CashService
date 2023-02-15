5d781ea97aec9729a1f9803602a6d467c438a3d3
ES\Pavel.Korsak <Pavel.Korsak@effective-soft.com>
2023-02-15 17:15:04 +0300

    Docker

--- src/CashService.GRPC/Program.cs
+++ src/CashService.GRPC/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args)
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
-var connectionString = builder.Configuration.GetConnectionString("PostgreSql");
+var connectionString = builder.Configuration.GetConnectionString("CashDb");
 
 builder.Services.AddPostgreSqlContext(options =>
 {
