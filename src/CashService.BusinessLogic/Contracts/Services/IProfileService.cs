de005f8c1f2c629bd28930c89554300812e86db4
bynikk <bynik2001@gmail.com>
2023-04-04 22:02:01 +0300

    implement profile service

---
+++ src/CashService.BusinessLogic/Contracts/Services/IProfileService.cs
@@ -0,0 +1,20 @@
+﻿using CashService.BusinessLogic.Entities;
+
+namespace CashService.BusinessLogic.Contracts.Services
+{
+    /// <summary>
+    /// Profile service contract.
+    /// </summary>
+    public interface IProfileService
+    {
+        /// <summary>
+        /// Creates the profile.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <param name="token">The token.</param>
+        /// <returns>
+        /// Task
+        /// </returns>
+        Task CreateProfile(Guid userId, CancellationToken token); 
+    }
+}
---
+++ src/CashService.BusinessLogic/Services/ProfileService.cs
@@ -0,0 +1,21 @@
+﻿using CashService.BusinessLogic.Contracts.Repositories;
+using CashService.BusinessLogic.Contracts.Services;
+using CashService.BusinessLogic.Entities;
+
+namespace CashService.BusinessLogic.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IProfileRepository _profileRepository;
+
+        public ProfileService(IProfileRepository profileRepository)
+        {
+            _profileRepository = profileRepository;
+        }
+
+        public async Task CreateProfile(Guid userId, CancellationToken token)
+        {
+            await _profileRepository.Add(new ProfileEntity() { Id = userId }, token);
+        }
+    }
+}
